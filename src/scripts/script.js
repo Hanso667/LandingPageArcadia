9f7527e6de0fa46f707d496be67a2ac13464c45d
xiruSilva <xiruuuu@gmail.com>
2025-05-29 21:07:47 -0300

    tenth commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,3 @@
-btnLogin = document.getElementById("Login")
-mnLogin = document.getElementById("Login-menu")
-
-function MenuOpen() {
-    if (mnLogin.classList.contains("open")) {
-        btnLogin.style.transform = "translateX(0)"
-        btnLogin.textContent = "Login"
-        mnLogin.classList.add('slideUp')
-        setTimeout(function () {
-            mnLogin.classList.remove('slideUp')
-            mnLogin.classList.remove("open")
-            mnLogin.classList.add("closed")
-        }, 500)
-    } else if (mnLogin.classList.contains("closed")) {
-        btnLogin.style.transform = "translateX(-110%)";
-        btnLogin.textContent = "X"
-        mnLogin.classList.remove("closed")
-        mnLogin.classList.add("open")
-    }
-}
 
 let imgs = ['Celeste', 'DarkSouls3', 'HollowKnight', 'Minecraft', 'Portal2', 'RedDead2', 'StardewValley', 'Zelda']
 
